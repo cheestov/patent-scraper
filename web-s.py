b3bb9f727e369d302417d8c598de5e4bbf0eb00c
Michael Tovey <cheestov@email.unc.edu>
2023-07-21 13:25:12 -0400

    added user agent file with 800 agents instead testing scrolling

--- web-s.py
+++ web-s.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 
 # basically scrape the the state id of the first assignee. N/A can be its own category I guess
@@ -13,36 +14,21 @@ from selenium.webdriver.chrome.service import Service
 
 pNumbers = []   # for storing patent numbers
 
-userAgents = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
-"Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2919.83 Safari/537.36",
-"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2866.71 Safari/537.36",
-"Mozilla/5.0 (X11; Ubuntu; Linux i686 on x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2820.59 Safari/537.36",
-"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2762.73 Safari/537.36",
-"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2656.18 Safari/537.36",
-"Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML like Gecko) Chrome/44.0.2403.155 Safari/537.36",
-"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
-"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36",
-"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
-"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
-"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2226.0 Safari/537.36",
-"Mozilla/5.0 (Windows NT 6.4; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36",
-"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36",
-"Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2224.3 Safari/537.36",
-"Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36",
-"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36",
-"Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36",
-"Mozilla/5.0 (Windows NT 4.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36",
-"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.67 Safari/537.36",
-"Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.67 Safari/537.36",
-"Mozilla/5.0 (X11; OpenBSD i386) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36"]
-agent = random.choice(userAgents)  # this is used to get past the webscraping protections. I tell the browser im 30 different users instead of 1
+filename = "user-agent-list.txt"
+lines = []
+with open(filename, "r") as f:
+   lines = f.readlines()
+agent = random.choice(lines).strip()
 
+#agent = random.choice(userAgents)  # this is used to get past the webscraping protections. I tell the browser im 30 different users instead of 1
+print(agent)
 service = Service()
 options = webdriver.ChromeOptions()
 options.add_argument("user-agent={}".format(agent))
+#options.add_argument('--proxy-server=198.199.70.20:31028')
 driver = webdriver.Chrome(service=service, options=options)  # object used to interact with chrome
 
-driver.get("https://ppubs.uspto.gov/pubwebapp/external.html?q=2012.fy.") # search results for patents files in 2012
+driver.get("https://ppubs.uspto.gov/pubwebapp/external.html?q=2013.fy.") # search results for patents files in 2012
 
 
 sleep(6)
@@ -50,11 +36,29 @@ chwd = driver.window_handles
 driver.switch_to.window(chwd[1])
 sleep(0.9)
 
-content = driver.page_source
-soup = BeautifulSoup(content)
-for a in soup.findAll('div', attrs={'class':'slick-cell l9 r9 left'}):
-    pNumber=a.find('button', attrs={'class':'results-key-cntrl btn-link'})
-    pNumbers.append(pNumber.text)
+
+
+
+inner_window = driver.find_element(By.CLASS_NAME, "slick-viewport")
+scroll = 0
+while scroll < 20:  # this will scroll 3 times
+    content = driver.page_source
+    soup = BeautifulSoup(content)
+    for a in soup.findAll('div', attrs={'class':'slick-cell l9 r9 left'}):
+        pNumber=a.find('button', attrs={'class':'results-key-cntrl btn-link'})
+        pNumbers.append(pNumber.text)
+    driver.execute_script('arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;', inner_window)
+    scroll += 1
+      # add appropriate wait here, of course. 1-2 seconds each
+    sleep(1)
+
+#content = driver.page_source
+#soup = BeautifulSoup(content)
+#for a in soup.findAll('div', attrs={'class':'slick-cell l9 r9 left'}):
+#    pNumber=a.find('button', attrs={'class':'results-key-cntrl btn-link'})
+ #   pNumbers.append(pNumber.text)
+    
+    
 
 
 df = pd.DataFrame({'Patent #':pNumbers}) 
