73a417220d5f489f602ced5b381b90ead45c788f
Michael Tovey <cheestov@email.unc.edu>
2023-07-21 11:22:08 -0400

    made it so selenium now goes to patent website still figuring out how to scrape it correctly for document ID # added a list of user-agents to get past scraping detection

--- web-s.py
+++ web-s.py
@@ -1,3 +1,5 @@
+import random
+from time import sleep
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -9,28 +11,49 @@ from selenium.webdriver.chrome.service import Service
 # patent number I think
 # if that doesnt take too long, use py stata to make a chart?
 
+pNumbers = []   # for storing patent numbers
 
+userAgents = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36",
+"Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2919.83 Safari/537.36",
+"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2866.71 Safari/537.36",
+"Mozilla/5.0 (X11; Ubuntu; Linux i686 on x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2820.59 Safari/537.36",
+"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2762.73 Safari/537.36",
+"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2656.18 Safari/537.36",
+"Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML like Gecko) Chrome/44.0.2403.155 Safari/537.36",
+"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
+"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36",
+"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
+"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.0 Safari/537.36",
+"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2226.0 Safari/537.36",
+"Mozilla/5.0 (Windows NT 6.4; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36",
+"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2225.0 Safari/537.36",
+"Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2224.3 Safari/537.36",
+"Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/40.0.2214.93 Safari/537.36",
+"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.124 Safari/537.36",
+"Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36",
+"Mozilla/5.0 (Windows NT 4.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36",
+"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.67 Safari/537.36",
+"Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.67 Safari/537.36",
+"Mozilla/5.0 (X11; OpenBSD i386) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36"]
+agent = random.choice(userAgents)
 
 service = Service()
 options = webdriver.ChromeOptions()
-driver = webdriver.Chrome(service=service, options=options)
+options.add_argument("user-agent={}".format(agent))
+driver = webdriver.Chrome(service=service, options=options)  # object used to interact with chrome
+
+driver.get("https://ppubs.uspto.gov/pubwebapp/external.html?q=2012.fy.") # search results for patents files in 2012
 
 
-products=[] #List to store name of the product
-prices=[] #List to store price of the product
-ratings=[] #List to store rating of the product
-driver.get("https://www.amazon.com/s?k=laptops&crid=3SJI526XFJYQQ&sprefix=laptop%2Caps%2C109&ref=nb_sb_noss_1")
-
+sleep(6)
 content = driver.page_source
 soup = BeautifulSoup(content)
-for a in soup.findAll('div', attrs={'class':'sg-col-inner'}):
-    name=a.find('span', attrs={'class':'a-size-medium a-color-base a-text-normal'})
-    products.append(name.text)
-    price=a.find('span', attrs={'class':'a-price-whole'})
-    prices.append(price.text)
+for a in soup.findAll('div', attrs={'class':'slick-cell l9 r9 left'}):
+    pNumber=a.find('button', attrs={'class':'results-key-cntrl btn-link'})
+    pNumbers.append(pNumber.text)
 
 
-df = pd.DataFrame({'Product Name':products, 'Product Price':prices}) 
+df = pd.DataFrame({'Patent #':pNumbers}) 
 df.to_csv('products.csv', index=False, encoding='utf-8')
 
 
